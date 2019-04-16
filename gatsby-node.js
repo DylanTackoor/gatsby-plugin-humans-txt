638ec7e9ccc8822f7be1548fecf657b84c20f67c
Dylan Tackoor <dylan@worldmedia.net>
2019-04-16 11:51:05 -0400

    removed unnecessary options filtering

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,13 +5,10 @@ const { join } = require(`path`)
 
 exports.onPostBuild = async (api, pluginOptions) => {
 	const filePath = join(`./public`, `humans.txt`)
-	const humansConfig = { ...pluginOptions }
-
-	delete humansConfig.plugins
 
 	try {
-		const humansTxt = await promisify(humans)(humansConfig)
-		await promisify(writeFile)(filePath, humansTxt.join(`\n`))
+		const humansTxt = await promisify(humans)(pluginOptions).join(`\n`)
+		await promisify(writeFile)(filePath, humansTxt)
 	} catch (error) {
 		console.error(error)
 		throw error
